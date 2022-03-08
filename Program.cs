f52cefab871f346db50dce1914fbefd9b71fc255
SAMUEL ALEJANDRO HURTADO JIMENEZ <samuelhurtado20@hotmail.com>
2022-03-07 23:16:09 -0500

    JWT token Login

--- Program.cs
+++ Program.cs
@@ -1,17 +1,79 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.IdentityModel.Tokens;
 using Minimal_API_Swagger_JWT.Interfaces;
 using Minimal_API_Swagger_JWT.Models;
 using Minimal_API_Swagger_JWT.Services;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSwaggerGen();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters()
+    { 
+        ValidateActor = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["JwtAudience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+    };
+});
+builder.Services.AddAuthorization();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSingleton<IMovieService, MovieService>();
 builder.Services.AddSingleton<IUserService, UserService>();
 
 var app = builder.Build();
 app.UseSwagger();
+app.UseAuthorization();
+app.UseAuthentication();
 
-app.MapPost("/create", (Movie movie, IMovieService service) =>
+app.MapPost("/login", (UserLogin user, IUserService service) =>
+{
+    if(!String.IsNullOrEmpty(user.Password) && !String.IsNullOrEmpty(user.UserName))
+    {
+        var logInUser = service.Get(user);
+        if (logInUser is null) return Results.NotFound("User not found");
+
+        var claims = new[]
+        {
+            new Claim(ClaimTypes.NameIdentifier, logInUser.Username ?? ""),
+            new Claim(ClaimTypes.Email, logInUser.EmailAddress ?? ""),
+            new Claim(ClaimTypes.GivenName, logInUser.GivenName ?? ""),
+            new Claim(ClaimTypes.Surname, logInUser.Surname ?? ""),
+            new Claim(ClaimTypes.Role, logInUser.Role ?? ""),
+        };
+
+        var token = new JwtSecurityToken
+        (
+            issuer: builder.Configuration["Jwt:Issuer"],
+            audience: builder.Configuration["Jwt:Audience"],
+            claims: claims,
+            expires: DateTime.UtcNow.AddMinutes(5),
+            notBefore: DateTime.UtcNow,
+            signingCredentials: new SigningCredentials(
+                new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"])),
+                SecurityAlgorithms.HmacSha256
+                )
+        );
+
+        var tokenString = new JwtSecurityTokenHandler().WriteToken(token);
+        return Results.Ok(tokenString);
+    }
+
+    return Results.NotFound("User not found");
+});
+
+
+app.MapPost("/create",
+[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "Administrator")]
+(Movie movie, IMovieService service) =>
 {
     var result = service.Create(movie);
     return Results.Ok(result);
